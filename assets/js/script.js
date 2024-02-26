0c23363513a671f746b12f54375a1cc79f672c29
K4WTY <108836220+K4WTY@users.noreply.github.com>
2024-02-26 16:24:51 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+const tituloColorido = document.getElementById('titulo-colorido')
+const subtitulo = document.getElementById('subtitulo')
+const conteudo = document.getElementById('conteudo')
+const botaoTras = document.getElementById('btn-one')
+const botaoFrente = document.getElementById('btn-two')
+const indice = document.getElementById('indice')
+const mainContent = document.getElementById('mainContent')
+const alterarImg = document.getElementById('alterarImg')
+
+let page = 1
+let backupContent = null
+
+indice.innerHTML = page
+
+function changeContent(title, subtitle, content) {
+    tituloColorido.innerHTML = title
+    subtitulo.innerHTML = subtitle
+    conteudo.innerHTML = content
+}
+
+function changeMainContent(content) {
+    let target = document.getElementById(content)
+    if (backupContent != null && content != backupContent) {
+        document.getElementById(backupContent).classList.add('d-none')
+    }
+    if (target.classList.contains('d-none')) {
+        target.classList.remove('d-none')
+    }
+    backupContent = content
+}
+
+function checkPage(numberPage) {
+    if (numberPage == 1) {
+        changeContent('Início do projeto', 'O que desenvolmemos?', 'O projeto foi criado para a área do aluno com o intuito de facilitar as necessidades básicas. A ideia do projeto foi desenvolvida pelos instrutores Marcelo Garcia e Bruno Wuo.')
+        changeMainContent('primeiroConteudo')
+    } else if (numberPage == 2) {
+        changeContent('Montagem da equipe', 'Como foi desenvolvido?', 'Para realizar o desenvolvimento do projeto foi necessário a criação de uma equipe que fosse capaz de concluir cada etapa, como, por exemplo, brainstorm, criar o esqueleto do site, protótipo, entre outras etapas...<br>Sabendo disso, a sala se reuniu e criou a equipe Seed Devs.')
+        changeMainContent('segundoConteudo')
+    } else if (numberPage == 3) {
+        changeContent('O começo', 'Brainstorm', 'Brainstorm é uma técnica que consiste em reunir duas ou mais pessoas com o objetivo de gerar novas ideias ou para debater sobre soluções.')
+        changeMainContent('terceiroConteudo')
+    } else if (numberPage == 4) {
+        changeContent('Esqueleto do site', 'Wireframe', 'Um wireframe web é uma ilustração semelhante do layout de elementos fundamentais na interface.<br>Ao lado está o wireframe do site na versão mobile.')
+        changeMainContent('quartoConteudo')
+    } else if (numberPage == 5) {
+        changeContent('Figma', 'Prototipação', 'Diferente do Wireframe, na prototipagem vamos criar o visual do site com as paletas de cores e definir os trajetos de cada botão.<br>As imagens ao lado ilustra a prototipagem de algumas telas.')
+        changeMainContent('quintoConteudo')
+    } else if (numberPage == 6) {
+        changeContent('Desenvolvimento', 'Mão na massa', 'Após as estruturas estabelecidas iniciou-se a etapa de transformar a ideia em algo funcional.<br>E para isso foi utilizada algumas tecnologias, como, HTML, CSS, JS, PHP e MySQL.<br><br>HTML: Linguagem de marcação.<br>CSS: Linguagem de estilização.<br>JS: Linguagem de programação.<br>PHP: Linguagem de programação.<br>MySQL: Gerenciamento de banco de dados.')
+        changeMainContent('sextoConteudo')
+    }
+}
+
+checkPage(page)
+
+botaoTras.addEventListener('click', function() {
+    if (page > 1) {
+        page--
+        checkPage(page)
+        indice.innerHTML = page
+        return
+    } if (page == 1) {
+        const recarregarPagina = confirm('Deseja recarregar a página?')
+        if (recarregarPagina) {
+            location.reload()
+        }
+    }
+})
+
+botaoFrente.addEventListener('click', function() {
+    if (page >= 1 && page < 6) {
+        page++
+        checkPage(page)
+        indice.innerHTML = page
+    }
+})
+
+alterarImg.addEventListener('click', function() {
+    if (this.innerHTML == 'Visual') {
+        document.getElementById('exemploCode').src =  './assets/imgs/Login_.png'
+        this.innerHTML = 'Código'
+        return
+    }
+    this.innerHTML = 'Visual'
+    document.getElementById('exemploCode').src =  './assets/imgs/vscode.gif'
+})
